cfcfcee506a187886030eada9a4ea9a7495476e7
Pedro R. Spidalieri <pedro.spidalieri@gmail.com>
2023-06-19 21:07:50 -0300

    Added studies & contact info.

--- scripts.js
+++ scripts.js
@@ -70,6 +70,9 @@ $(document).ready(function(){
     //HOME
     const bookIcon = "<svg xmlns='http://www.w3.org/2000/svg' class='section-icon' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25' /></svg>";
     const briefCaseIcon = "<svg xmlns='http://www.w3.org/2000/svg' class='section-icon' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' d='M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z' /></svg>";
+    const envelopeIcon = "<svg xmlns='http://www.w3.org/2000/svg' class='section-icon' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75' /></svg>";
+    const academyIcon = "<svg xmlns='http://www.w3.org/2000/svg' class='section-icon' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5' /></svg>";
+
     $('#title-label').text(language.home.full_title_label);
     $('#title-text').text(language.home.full_title);
     $('#country-label').text(language.home.country_label);
@@ -90,6 +93,18 @@ $(document).ready(function(){
     $('#layers').text(language.home.knowledge.methodologies.layers);
     $('#mvc').text(language.home.knowledge.methodologies.mvc);
     $('#wpf').text(language.home.knowledge.methodologies.wpf);
+
+    //STUDIES
+    $('#studies-title').html(academyIcon + language.home.studies.studies_title);
+    $('#st-degree-label').text(language.home.studies.st_degree_label);
+    $('#st-degree-text').text(language.home.studies.st_degree_text);
+    $('#graduate-label').text(language.home.studies.graduate_label);
+    $('#graduate-text').text(language.home.studies.graduate_text);
+    $('#gt-year').text(language.home.studies.st_year);
+    $('#st-year-label').text(language.home.studies.st_year);
+    $('#mba-degree-label').text(language.home.studies.mba_degree_label);
+    $('#mba-degree-text').text(language.home.studies.mba_degree_text);
+    $('#mba-year-label').text(language.home.studies.st_year);
 
     //EXPERIENCE
     $('#experience').html(briefCaseIcon + language.home.experience.experience_title);
@@ -128,6 +143,11 @@ $(document).ready(function(){
     $('#wc-IDEs').text(language.projects.IDEs_tools);
     $('#wc-description').text(language.projects.wc_description);
 
+    //CONTACT
+    $('#contact-title').html(envelopeIcon + language.contact.contact_title);
+    $('#email').text(language.contact.email);
+    $('#phone').text(language.contact.phone);
+    $('#social-media').text(language.contact.social_media)
 
     //HIGHLIGHTED OPTIONS
     if (localStorage.getItem('language')==='es') {
@@ -150,6 +170,8 @@ $(document).ready(function(){
     $('#knowledge-section').addClass('visible');
     $('#experience-section').addClass('visible');
     $('#projects-section').addClass('visible');
+    $('#contact-section').addClass('visible');
+    $('#studies-section').addClass('visible');
 
     // SET THEME
     (localStorage.getItem('theme') === 'light') ? $('body').addClass('light')
